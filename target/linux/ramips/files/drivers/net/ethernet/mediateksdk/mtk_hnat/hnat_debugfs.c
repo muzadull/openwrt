eb8567f79f745eb5bea583c6cbc92c9b912835d0
muzadull <muzadull@gmail.com>
2024-05-01 03:57:44 +0400

    Update hnat_debugfs.c

--- target/linux/ramips/files/drivers/net/ethernet/mediateksdk/mtk_hnat/hnat_debugfs.c
+++ target/linux/ramips/files/drivers/net/ethernet/mediateksdk/mtk_hnat/hnat_debugfs.c
@@ -3277,7 +3277,6 @@ int hnat_init_debugfs(struct mtk_hnat *h)
 {
 	int ret = 0;
 	struct dentry *root;
-	struct dentry *file;
 	long i;
 	char name[16];
 
@@ -3305,13 +3304,8 @@ int hnat_init_debugfs(struct mtk_hnat *h)
 			ret = -ENOMEM;
 			goto err1;
 		}
-		file = debugfs_create_regset32(name, 0444,
+		debugfs_create_regset32(name, 0444,
 					       root, h->regset[i]);
-		if (!file) {
-			dev_notice(h->dev, "%s:err at %d\n", __func__, __LINE__);
-			ret = -ENOMEM;
-			goto err1;
-		}
 	}
 
 	debugfs_create_file("all_entry", 0444, root, h, &hnat_debug_fops);
