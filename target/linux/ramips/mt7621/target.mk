b53b8c369d5680085036b7ee58714334273bd806
muzadull <muzadull@gmail.com>
2024-08-01 12:49:02 +0400

    Update target.mk

--- target/linux/ramips/mt7621/target.mk
+++ target/linux/ramips/mt7621/target.mk
@@ -10,7 +10,7 @@ KERNELNAME:=vmlinux vmlinuz
 # make Kernel/CopyImage use $LINUX_DIR/vmlinuz
 IMAGES_DIR:=../../..
 
-DEFAULT_PACKAGES += wpad-basic-mbedtls uboot-envtools kmod-ramips_hnat
+DEFAULT_PACKAGES += wpad-basic-mbedtls uboot-envtools kmod-ramips_hnat kmod-br-netfilter kmod-ipt-ipopt iptables-mod-extra
 
 define Target/Description
 	Build firmware images for Ralink MT7621 based boards.
