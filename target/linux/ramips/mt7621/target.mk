e05c6d5839f14466c47a71fe4b34fd63020241a5
muzadull <muzadull@gmail.com>
2024-08-04 19:44:02 +0400

    Customised Default Packages

--- target/linux/ramips/mt7621/target.mk
+++ target/linux/ramips/mt7621/target.mk
@@ -10,7 +10,10 @@ KERNELNAME:=vmlinux vmlinuz
 # make Kernel/CopyImage use $LINUX_DIR/vmlinuz
 IMAGES_DIR:=../../..
 
-DEFAULT_PACKAGES += wpad-basic-mbedtls uboot-envtools
+DEFAULT_PACKAGES += wpad-basic-mbedtls uboot-envtools \
+                    kmod-ip6tables kmod-ipt-conntrack kmod-ipt-extra kmod-ipt-nat kmod-br-netfilter kmod-ipt-offload kmod-sched-connmark kmod-veth \
+                    iptables-mod-conntrack-extra iptables-zz-legacy ip6tables-zz-legacy iptables-mod-ipopt \
+                    luci luci-app-adblock luci-app-https-dns-proxy luci-app-sqm luci-app-ttyd curl ethtool irqbalance
 
 define Target/Description
 	Build firmware images for Ralink MT7621 based boards.
